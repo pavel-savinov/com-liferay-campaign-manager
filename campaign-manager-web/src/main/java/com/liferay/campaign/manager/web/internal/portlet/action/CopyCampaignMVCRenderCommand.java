65e98d3005883e57762d8ad71202b5c7761906fa
Pavel Savinov <pavel.savinov@liferay.com>
2016-09-28 11:16:13 +0200

    CM-1 campaign edit and copy views and actions

---
+++ campaign-manager-web/src/main/java/com/liferay/campaign/manager/web/internal/portlet/action/CopyCampaignMVCRenderCommand.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.campaign.manager.web.internal.portlet.action;
+
+import com.liferay.campaign.manager.web.internal.constants.CampaignManagerPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + CampaignManagerPortletKeys.CAMPAIGN_MANAGER,
+		"mvc.command.name=/campaign_manager/copy_campaign"
+	},
+	service = MVCRenderCommand.class
+)
+public class CopyCampaignMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		renderRequest.setAttribute("copyCampaign", true);
+
+		return "/edit_campaign.jsp";
+	}
+
+}
